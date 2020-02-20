5f57793aa3b770df81daac108fe86e9a5ef7f3f4
Meatball <mainmeatball1@gmail.com>
2020-02-21 01:10:54 +0300

    Add authorization routes for user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
     {
         path: 'messages',
         loadChildren: () => import('./pages/messages/messages.module').then(m => m.MessagesModule)
-    }
+    },
+    {
+        path: 'sign-up',
+        loadChildren: () => import('./pages/sign/sign.module').then(s => s.SignModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then(s => s.LoginModule)
+    },
 ];
