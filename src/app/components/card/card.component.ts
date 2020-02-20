2623a0ca2fda00f7caca84f00636dfa2b94bf713
Meatball <mainmeatball1@gmail.com>
2020-02-21 01:07:09 +0300

    Update card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,35 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output} from '@angular/core';
+import {BlogMessage} from '../../models/blog-message.interface';
+import {MessagesService} from '../../pages/messages/services/messages.service';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css'],
+    styleUrls: ['./card.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent {
+    @Input() item: BlogMessage;
+
+    public isEditMode = false;
+
+    constructor(
+      private messages: MessagesService,
+      private cdr: ChangeDetectorRef
+    ) {
+
+    }
+
+    public upvote(): void {
+      this.messages.upvoteMessage(this.item.id).subscribe(
+        () => {
+          this.item.likes += 1;
+          this.cdr.markForCheck();
+        }
+      );
+    }
+
+    public toggleEditMode(): void {
+      this.isEditMode = !this.isEditMode;
+    }
 }
