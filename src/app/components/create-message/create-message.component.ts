69a1138f3e7ca128da77c53243befa0c08f79e69
Meatball <mainmeatball1@gmail.com>
2020-02-25 02:17:28 +0300

    Implement ability to add tags to new message

--- src/app/components/create-message/create-message.component.ts
+++ src/app/components/create-message/create-message.component.ts
@@ -1,30 +1,42 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 import {BlogMessage} from '../../models/blog-message.interface';
 import {MessagesService} from '../../pages/messages/services/messages.service';
+import {Tag} from '../../models/tag.interface';
 
 @Component({
   selector: 'app-create-message',
   templateUrl: './create-message.component.html',
-  styleUrls: ['./create-message.component.css'],
+  styleUrls: ['./create-message.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateMessageComponent {
-  @Output() postMessage = new EventEmitter<BlogMessage>();
+    @Output() postMessage = new EventEmitter<BlogMessage>();
 
-  public readonly messageForm = new FormGroup({
-    content: new FormControl('')
-  });
+    public readonly messageForm = new FormGroup({
+        content: new FormControl(''),
+        tags: new FormArray([])
+    });
 
-  constructor(private messagesService: MessagesService) {
-  }
+    public readonly tagForm = new FormGroup({
+        name: new FormControl('')
+    });
 
-  public add(): void {
-      this.messagesService.submitMessage(this.messageForm.value).subscribe(
-      (message: BlogMessage) => {
-        this.postMessage.emit(message);
-      }
-    );
-  }
+    constructor(private messagesService: MessagesService) {
+    }
+
+    public add(): void {
+        this.messagesService.submitMessage(this.messageForm.value).subscribe(
+            (message: BlogMessage) => {
+                this.postMessage.emit(message);
+            }
+        );
+    }
+
+    public addTag(): void {
+        const tags = this.messageForm.get('tags') as FormArray;
+        const newTag: Partial<Tag> = {name: this.tagForm.get('name').value};
+        tags.push(new FormControl(newTag));
+    }
 
 }
--- src/app/components/create-message/create-message.module.ts
+++ src/app/components/create-message/create-message.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateMessageComponent } from './create-message.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TagModule} from '../tag/tag.module';
 
 
 @NgModule({
   declarations: [CreateMessageComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TagModule
+    ],
     exports: [
         CreateMessageComponent
     ]
