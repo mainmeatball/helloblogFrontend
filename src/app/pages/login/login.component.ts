8d54b6d51c35fca8013f7b2694c4f6a462c87e55
Meatball <mainmeatball1@gmail.com>
2020-02-25 23:31:19 +0300

    Make login redirect to main page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,9 +2,10 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {UserService} from './services/user.service';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Token} from './models/token';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-sign',
+  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,13 +16,17 @@ export class LoginComponent {
         password: new FormControl('')
     });
 
-    constructor(private userService: UserService) {
+    constructor(
+        private router: Router,
+        private userService: UserService
+    ) {
     }
 
-    login() {
+    login(): void {
         this.userService.login(this.userForm.value).subscribe(
             ({id_token}: Token) => {
                 localStorage.setItem('id_token', id_token);
+                this.router.navigate(['/messages']);
             }
         );
     }
