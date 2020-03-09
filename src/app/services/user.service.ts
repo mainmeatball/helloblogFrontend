5f3391458f0c3dc2cd9a0107e00d9bc8dcb6eb59
Meatball <mainmeatball1@gmail.com>
2020-03-09 21:39:50 +0300

    Add user service for showing which user is logged in

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,36 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-
-
+import {BehaviorSubject, Observable} from 'rxjs';
+import {TokenUser} from '../models/token-user.interface';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UserService {
-    constructor(private http: HttpClient) {
+
+    public readonly logged$ = new BehaviorSubject<TokenUser>({auth: [], sub: ''});
+
+    public initialize() {
+        const token = localStorage.getItem('id_token');
+        if (token) {
+            const user: TokenUser = jwt_decode(token);
+            this.logged$.next(user);
+        } else {
+            this.logged$.next({sub: '', auth: []});
+        }
+    }
+
+    public userChanges(): Observable<TokenUser> {
+        return this.logged$.asObservable();
+    }
+
+    public setToken(jwt: string) {
+        localStorage.setItem('id_token', jwt);
+        this.initialize();
+    }
+
+    public removeToken() {
+        localStorage.removeItem('id_token');
+        this.initialize();
     }
 }
