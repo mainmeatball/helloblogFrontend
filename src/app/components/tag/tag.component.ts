66affc97b756bcfe79fabba0b90311c228bc9385
Meatball <mainmeatball1@gmail.com>
2020-03-09 21:40:59 +0300

    Add tags class for being able to update them

--- src/app/components/tag/tag.component.ts
+++ src/app/components/tag/tag.component.ts
@@ -1,5 +1,9 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges} from '@angular/core';
 import {Tag} from '../../models/tag.interface';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {angularLifecycleMethodKeys} from 'codelyzer/util/utils';
 
 @Component({
     selector: 'app-tag',
@@ -7,6 +11,30 @@ import {Tag} from '../../models/tag.interface';
     styleUrls: ['./tag.component.css'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TagComponent {
+export class TagComponent implements OnChanges {
     @Input() item: Tag;
+
+    private readonly name$ = new BehaviorSubject<string>('');
+
+    public readonly queryParam$ = this.queryParamsChanges();
+
+    constructor(private activatedRoute: ActivatedRoute) {
+    }
+
+    private queryParamsChanges(): Observable<string> {
+        return combineLatest([
+            this.name$,
+            this.activatedRoute.queryParamMap
+        ])
+            .pipe(
+                map(([name, paramMap]: [string, ParamMap]) => !paramMap.has('tags')
+                    ? name
+                    : `${this.item.name},${paramMap.get('tags')}`
+                )
+            );
+    }
+
+    ngOnChanges(): void {
+        this.name$.next(this.item.name);
+    }
 }
--- src/app/components/tag/tag.module.ts
+++ src/app/components/tag/tag.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagComponent } from './tag.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [TagComponent],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
     exports: [
         TagComponent
     ]
